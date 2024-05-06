34ad0fe9e55b8351d282dc92e4e9e07300f1fc14
Max <maxorteu@gmail.com>
2024-05-06 23:43:12 +0200

    Fixing Central Scheme; Fixing Periodic Boundary Conditions (added ghost points for Central)

--- MMCM_Exe2_20240506.m
+++ MMCM_Exe2_20240506.m
@@ -1,12 +1,11 @@
 clc;
 
-L = 10; % For space interval [0,L]
-T = 30; % For time interval [0,T]
-nL = 50; % For nL spaces in array Xs
-nT = 10; %For nT spaces in array Xs
+L = 1; % For space interval [0,L]
+T = 1; % For time interval [0,T]
+nL = 200; % For nL spaces in array Xs
+nT = 500; %For nT spaces in array Ts
 k = 15;  % Some integer k
 w = -15; % Some integer w
-
 
 
 % Our functions
@@ -19,76 +18,47 @@ g = @(x,t) sin((2*k*pi*x)/L-w*t);
 aux = linspace(0,L,nL+1);
 Xs = linspace(0,L,nL);
     for i = 1:nL
-    
         Xs(i) = (aux(i)+aux(i+1))/2;
-        fprintf('%d \t', Xs(i))
-    
     end 
-fprintf('\n')
 
 % Discrete increment of space
 dx = Xs(2)-Xs(1);
 
 % Array to represent the images of a function applied to the values of Xs
-Fs = linspace(0,L,nL);
-    for i = 1:nL
-    
-        Fs(i) = f(Xs(i));
-        fprintf('%d \t', Fs(i))
-    
-    end 
-
-fprintf('\n%i\n',length(Xs))
-
-
+Fs = f(Xs);
 
 
 % ----- DOWNWIND METHOD -----
 
-
-
 % (Time) Array representing the center of n intervals of lentgh T/nT
 aux = linspace(0,T,nT+1);
-Ts = linspace(0,T,nT);
+Ts = zeros(1,nT);
     for i = 1:nT
-    
         Ts(i) = (aux(i)+aux(i+1))/2;
-    
     end 
-fprintf('\n')
 
 % Discrete increment of time
 dt = Ts(2)-Ts(1); 
 
 % Creating another auxiliar array, now of the same dimension as Fs, this
 % will help us to go forward in time on each iteration
-aux = linspace(0,L,nL);
+aux = zeros(1,nL);
     for i = 1:nL
-    
         aux(i) = Fs(i);
-    
     end
 
 % implementation of the downwind method with constant c = 1
 for j = 1:nT
-
     for i = 1:nL-1
-
         Fs(i) = aux(i) - (dt/dx)*(aux(i+1)-aux(i));
-
     end
 
-    Fs(nL) = aux(nL) - (dt/dx)*(aux(1)-aux(nL)); %Final step on the iretation
-
+    Fs(nL) = Fs(1); % periodic boundary condition
 end
-
 
 figure(1);
 plot(Xs, Fs);
-title('Aproximation via "Downwind method" of te function $sin((2*k*pi*x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
-
-
-
+title('Aproximation via "Downwind method" of the function $\sin((2k\pi x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
 
 
 % ----- UPWIND METHOD -----
@@ -97,28 +67,19 @@ title('Aproximation via "Downwind method" of te function $sin((2*k*pi*x)/L-t);$'
 Fs = f(Xs);
 aux = Fs;
 
-
 % implementation of the upwind method with constant c = 1
 for j = 1:nT
-
     for i = 2:nL
-
         Fs(i) = aux(i) - (dt/dx)*(aux(i)-aux(i-1));
-
     end
 
-    Fs(1) = aux(1) - (dt/dx)*(aux(1)-aux(nL)); %Final step on the iretation
-
+    Fs(1) = Fs(nL); % periodic boundary condition
 end
 
 
 figure(2);
 plot(Xs, Fs);
-title('Aproximation via "Upwind method" of te function $sin((2*k*pi*x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
-
-
-
-
+title('Aproximation via "Upwind method" of te function $\sin((2k\pi x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
 
 
 % ----- CENTRAL METHOD -----
@@ -130,21 +91,19 @@ aux = Fs;
 
 % implementation of the central method with constant c = 1
 for j = 1:nT
-
     for i = 2:nL-1
-
-        Fs(i) = aux(i) - (dt/dx)*(aux(i+1)-aux(i-1));
-
+        Fs(i) = aux(i) - (dt/(2*dx))*(aux(i+1)-aux(i-1));
     end
 
-    %Final steps on the iretation
-    Fs(1) = aux(1) - (dt/dx)*(aux(1)-aux(nL)); 
-
+    % add ghost points for boundary using periodicity phi(t,x+L)=phi(t,x)
+    gh1 = Fs(end-1);
+    gh2 = Fs(2);
+    Fs(1) = aux(1) - (dt/(2*dx))*(aux(2)-gh1);
+    Fs(nL) = aux(nL) - (dt/(2*dx))*(gh2-aux(nL-1));
 end
 
-
-figure(2);
+figure(3);
 plot(Xs, Fs);
-title('Aproximation via "Upwind method" of te function $sin((2*k*pi*x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
+title('Aproximation via "Central method" of te function $\sin((2k\pi x)/L-t);$', 'interpreter', 'latex', 'FontSize', 14);
 
 
